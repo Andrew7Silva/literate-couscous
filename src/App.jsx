dd7a976d2d4f5f4e1e759cc13def5a203a4da78a
Andrew7Silva <reginaldoandrew07@gmail.com>
2022-08-15 22:27:56 -0300

    Validação e Função finalizar tarefa

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 function App() {
   const [task, setTask] = useState("")
   const [tempo, setTempo] = useState(0)
-  const [lisTarefas, setLisTarefas] = useState([])
+  const [listaTarefas, setListaTarefas] = useState([])
   
   
   //apagar uma nota
@@ -15,23 +15,34 @@ function App() {
       tempo
     }
 
-    setLisTarefas([...lisTarefas, novaTarefa])
-  
-  
+    if (!task || tempo<= 0){
+    alert ("Valores inválidos")
+    }
+    else {
+      setListaTarefas([...listaTarefas, novaTarefa])
+      setTask("")
+      setTempo(0)
+    }
+    
   }
 
 
   // pra escutar os inputs
   function handleInputs(evento){
-    if(evento.target.name = "task"){
+    if(evento.target.name == "task"){
       //console.log(evento.target.value)
        setTask(evento.target.value)
     }else{
       //console.log(evento.target.value)
-      setTempo(evento.tempo.value)
+      setTempo(Number(evento.target.value))
       
     }
   }
+
+  function finalizarTarefa (nomeTarefa){
+    setListaTarefas(listaTarefas.filter(tarefa => tarefa.task != nomeTarefa))
+  }
+
 
   return (
    
@@ -42,20 +53,28 @@ function App() {
 
     
     <label>Nota</label>
-    <input name='task' onChange={handleInputs} type="text" />
+    <input 
+    value={task}  
+    name='task' 
+    onChange={handleInputs} 
+    type="text" />
 
     <label>Tempo</label>
-    <input name='tempo' onChange={handleInputs} type="number"/>
-    <button onClick={adiciornarNota}>Adicionar Notas</button>
+    <input 
+    value={tempo} 
+    name='tempo' 
+    onChange={handleInputs} 
+    type="number"/>
+    <button onClick={adicionarNota}>Adicionar Notas</button>
     
     
     { listaTarefas.map((value, index)=>{
     return(
-     
-      <div>
+      <div key={index}>
          <h3>{value.task}</h3>
          <h3>{value.tempo}</h3>
-
+         <button onClick = {() => {finalizarTarefa(value.task)}} >Finalizar Tarefa</button>
+         <h2>-------------</h2>
       </div>
     )
 
